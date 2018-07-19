b039ccd6ae73acd531d9f51eb41707302bf20dcc
suzuki84g <>
2018-07-20 00:42:54 +0900

    ガチマの取得してた。リグマが良い。

--- src/jsonGet.js
+++ src/jsonGet.js
@@ -1,5 +1,5 @@
 const getJson = new XMLHttpRequest();   // XMLHttpRequest オブジェクトを生成する
-getJson.open("GET", "https://spla2.yuu26.com/gachi/now", false);  // HTTPメソッドとアクセスするサーバーの　URL　を指定
+getJson.open("GET", "https://spla2.yuu26.com/league/now", false);  // HTTPメソッドとアクセスするサーバーの　URL　を指定
 getJson.send(null);   // 実際にサーバーへリクエストを送信
 
 const jsonData = JSON.parse(getJson.responseText)   //jsonデータを導入してパース
