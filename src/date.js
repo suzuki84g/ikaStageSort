2710ae20e6c172f7becc4cd0c076573301f8f339
suzuki84g <>
2018-07-20 20:34:20 +0900

    jsの読み込み位置修正の為

--- src/date.js
+++ src/date.js
@@ -1,14 +1,16 @@
 const date = new Date();
 
+//日付取得
 const year = date.getFullYear();
 const month = date.getMonth()+1;  //1月が0、12月が11。そのため+1をする。
 const day = date.getDate();
-
 const hour = date.getHours();
 const minute = date.getMinutes();
 const second = date.getSeconds();
 const time = date.getDate();
 
-const today = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second+' '
+//日付作成
+const print = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second+' '
 
-document.write(today+'に取得したステージ情報は以下の通りです。');
+const pri = document.getElementById("sentence")
+pri.innerHTML = print+"に取得したステージ情報です。"
